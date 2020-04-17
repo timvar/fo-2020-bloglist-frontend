3d5aeadb71cac0fe660294d415c6ff392584ccad
Timo Varila <timo.varila@siili.com>
2020-04-17 15:50:18 +0300

    5.18 step2

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -15,8 +15,25 @@ describe('Blog app', function() {
     cy.contains('Blog application')
   })
 
-  it('login form can be opened', function() {
+  it('login form is shown and can be opened', function() {
+    cy.contains('log in to application')
     cy.contains('login').click()
+  })
+
+  describe('login', function(){
+    it('succeeds with correct credentials', function(){
+      cy.get('#username').type('testaaja')
+      cy.get('#password').type('baller23')
+      cy.contains('login').click()
+      cy.contains('King James logged in')
+    })
+
+    it('fails with incorrect credentials and gives error message', function(){
+      cy.get('#username').type('foo')
+      cy.get('#password').type('bar')
+      cy.contains('login').click()
+      cy.contains('wrong username or password')
+    })
   })
 })
 
