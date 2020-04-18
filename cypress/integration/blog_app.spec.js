9f29221cf87af6208ff4057bff0900a721f0e6c2
Timo Varila <timo.varila@siili.com>
2020-04-18 16:38:51 +0300

    5.20 step4

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     const user = {
       name: 'King James',
@@ -11,24 +11,24 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
   })
 
-  it('front page can be opened', function() {
+  it('front page can be opened', function () {
     cy.contains('Blog application')
   })
 
-  it('login form is shown and can be opened', function() {
+  it('login form is shown and can be opened', function () {
     cy.contains('log in to application')
     cy.contains('login').click()
   })
 
-  describe('login', function(){
-    it('succeeds with correct credentials', function(){
+  describe('login', function () {
+    it('succeeds with correct credentials', function () {
       cy.get('#username').type('testaaja')
       cy.get('#password').type('baller23')
       cy.get('#loginbutton').click()
       cy.contains('King James logged in')
     })
 
-    it('fails with incorrect credentials and gives error message', function(){
+    it('fails with incorrect credentials and gives error message', function () {
       cy.get('#username').type('foo')
       cy.get('#password').type('bar')
       cy.get('#loginbutton').click()
@@ -36,20 +36,27 @@ describe('Blog app', function() {
     })
   })
 
-  describe.only('When logged in', function() {
-    beforeEach(function() {
-      cy.get('#username').type('testaaja')
-      cy.get('#password').type('baller23')
-      cy.get('#loginbutton').click()
+  describe.only('When logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'testaaja', password: 'baller23' })
+      cy.createBlog({
+        title: 'foo',
+        author: 'bar',
+      })
     })
 
-    it('blog can be created', function() {
+    it('blog can be created', function () {
       cy.contains('create new blog').click()
       cy.get('#title').type('foo')
       cy.get('#author').type('bar')
       cy.get('#url').type('https://baz.fi')
       cy.get('#create').click()
       cy.contains('foo bar')
+    })
+
+    it('blog can be liked', function () {
+      cy.contains('view').click()
+      cy.contains('like').click()
     })
   })
 })
